90d00cb2cf6a5d643cbff45b941a1253937a5980
Jonathan <jonathansawers@icloud.com>
2025-06-20 04:35:43 +0000

    [automatic commit] 2025-06-20 04:35:43 UTC 220.253.77.87

--- part2/app.js
+++ part2/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 var mysql = require('mysql2/promise');
 require('dotenv').config();
 
