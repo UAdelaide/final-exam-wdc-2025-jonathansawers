427b3955157ad59216a31664c88d9ca32a1b0e7b
Jonathan <jonathansawers@icloud.com>
2025-06-20 04:23:09 +0000

    [automatic commit] 2025-06-20 04:23:09 UTC 220.253.77.87

--- part2/app.js
+++ part2/app.js
@@ -13,8 +13,32 @@ app.use(express.static(path.join(__dirname, '/public')));
 const walkRoutes = require('./routes/walkRoutes');
 const userRoutes = require('./routes/userRoutes');
 
+// copied from pt1
 let db;
+(async () => {
+  try {
+    // Connect to MySQL without specifying a database
+    const connection = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '' // Set your MySQL root password
+    });
 
+    // Create the database if it doesn't exist
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
+    await connection.end();
+
+    // Now connect to the created database
+    db = await mysql.createConnection({
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'DogWalkService'
+    });
+  } catch (err) {
+    console.error('Error setting up database. Ensure Mysql is running: service mysql start', err);
+  }
+})();
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
