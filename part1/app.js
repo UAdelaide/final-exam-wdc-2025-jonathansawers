ff3546db28540104a6096586e1bb557138c8f682
Jonathan <jonathansawers@icloud.com>
2025-06-20 02:28:40 +0000

    [automatic commit] 2025-06-20 02:28:40 UTC 220.253.77.87

--- part1/app.js
+++ part1/app.js
@@ -41,7 +41,12 @@ let db;
     const [userCount] = await db.execute('SELECT COUNT(*) AS count FROM users');
     if (userCount[0].count === 0) {
         await db.execute(`
-
+            INSERT INTO Users (username, email, password_hash, role) VALUES
+            ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+            ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+            ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+            ('jono', 'jono@jono.com', 'hashedpassword', 'owner'),
+            ('pratik', 'tutor@adelaide.edu.au', 'hashed1234', 'owner')
             `);
     }
 
