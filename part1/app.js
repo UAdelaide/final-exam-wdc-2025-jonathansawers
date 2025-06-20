f311a7254b0ad94d8b889082246ed08da803c0a4
Jonathan <jonathansawers@icloud.com>
2025-06-20 02:11:52 +0000

    [automatic commit] 2025-06-20 02:11:52 UTC 220.253.77.87

--- part1/app.js
+++ part1/app.js
@@ -83,7 +83,9 @@ app.get('/api/walkrequests/open', async (req, res) => {
     try {
         const [results] = await db.execute(`
             SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
-            
+            FROM WalkRequests wr
+            JOIN Dogs d ON wr.dog_id = d.dog_id
+            JOIN Users u ON d.
             `);
         res.json(results);
     } catch (err) {
