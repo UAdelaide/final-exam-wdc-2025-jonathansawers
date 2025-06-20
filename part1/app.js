13474030dfc951abb86948987e757dc792c155b2
Jonathan <jonathansawers@icloud.com>
2025-06-20 02:29:41 +0000

    [automatic commit] 2025-06-20 02:29:41 UTC 220.253.77.87

--- part1/app.js
+++ part1/app.js
@@ -49,7 +49,14 @@ let db;
             ('pratik', 'tutor@adelaide.edu.au', 'hashed1234', 'owner')
             `);
 
-        const 
+        await db.execute(`
+            INSERT INTO Dogs (owner_id, name, size) VALUES
+            ((SELECT user_id FROM Users WHERE username='alice123'), 'Max', 'medium'),
+            ((SELECT user_id FROM Users WHERE username='carol123'), 'Bella', 'small'),
+            ((SELECT user_id FROM Users WHERE username='jono'), 'Rossie', 'small'),
+            ((SELECT user_id FROM Users WHERE username='pratik'), 'Fernando', 'large'),
+            ((SELECT user_id FROM Users WHERE username='bobwalker'), 'Hamilton', 'medium');
+            `);
     }
 
   } catch (err) {
